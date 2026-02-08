8c4e7a536241fbff7455ec165fd1d981f296928b
William Alves <willoandrade29@gmail.com>
2026-02-08 20:19:29 -0300

    tests: adiciona testes de arquitetura

---
+++ MSAProject.Tests/Arquitetura/ArquiteturaTests.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using MSAProject.Application.Interfaces;
+using MSAProject.Domain.Cliente;
+using MSAProject.Infrastructure;
+using NetArchTest.Rules;
+using Shouldly;
+
+namespace MSAProject.Tests.Arquitetura;
+
+public class ArquiteturaTests
+{
+    private static readonly Assembly AssemblyDominio = typeof(Cliente).Assembly;
+    private static readonly Assembly AssemblyAplicacao = typeof(ICommandHandler).Assembly;
+    private static readonly Assembly AssemblyInfraestrutura = typeof(DependencyInjection).Assembly;
+    
+    public static IEnumerable<object[]> CamadasIncompativeis => new List<object[]>
+    {
+        new object[] { AssemblyDominio, "MSProject.Api" },
+        new object[] { AssemblyDominio, AssemblyAplicacao.GetName().Name },
+        new object[] { AssemblyDominio, AssemblyInfraestrutura.GetName().Name },
+        new object[] { AssemblyAplicacao, AssemblyInfraestrutura.GetName().Name },
+        new object[] { AssemblyAplicacao, "MSProject.Api" },
+        new object[] { AssemblyInfraestrutura, "MSProject.Api" },
+    };
+
+    [Theory]
+    [MemberData(nameof(CamadasIncompativeis))]
+    public void CamadaInterna_NaoDeveReferenciar_CamadaExterna(Assembly camadaInterna, string camadaExterna)
+    {
+        var result = Types
+            .InAssembly(camadaInterna)
+            .ShouldNot()
+            .HaveDependencyOn(camadaExterna)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+}
