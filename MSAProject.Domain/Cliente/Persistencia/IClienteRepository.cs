371275022c040d87218aaab902b64be6f6c3b3f7
William Alves <willoandrade29@gmail.com>
2026-02-09 23:51:36 -0300

    refactor: adiciona cancellation tokens nos métodos do repositório

--- MSAProject.Domain/Cliente/Persistencia/IClienteRepository.cs
+++ MSAProject.Domain/Cliente/Persistencia/IClienteRepository.cs
@@ -2,6 +2,6 @@ namespace MSAProject.Domain.Cliente.Persistencia;
 
 public interface IClienteRepository
 {
-    Task<Cliente?> ObtemClientePorIdAsync(int id);
-    Task CriarClienteAsync(Cliente cliente);
+    Task<Cliente?> ObtemClientePorIdAsync(int id, CancellationToken token);
+    Task CriarClienteAsync(Cliente cliente, CancellationToken token);
 }
