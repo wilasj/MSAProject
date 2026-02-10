59923e3104c87a5fb3613e78e9a1a8799b451267
William Alves <willoandrade29@gmail.com>
2026-02-09 22:58:46 -0300

    tests: atualiza testes pra projeto Common

--- MSAProject.Tests/Arquitetura/ArquiteturaTests.cs
+++ MSAProject.Tests/Arquitetura/ArquiteturaTests.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using MSAProject.Application.Interfaces;
+using MSAProject.Common;
 using MSAProject.Domain.Cliente;
 using MSAProject.Infrastructure;
 using NetArchTest.Rules;
@@ -12,7 +13,11 @@ public class ArquiteturaTests
     private static readonly Assembly AssemblyDominio = typeof(Cnpj).Assembly;
     private static readonly Assembly AssemblyAplicacao = typeof(ICommand).Assembly;
     private static readonly Assembly AssemblyInfraestrutura = typeof(DependencyInjection).Assembly;
+    private static readonly Assembly AssemblyComum = typeof(Resultado).Assembly;
     
+    /// <summary>
+    /// Camadas que não devem ter dependências, da esquerda pra direita.
+    /// </summary>
     public static IEnumerable<object[]> CamadasIncompativeis => new List<object[]>
     {
         new object[] { AssemblyDominio, "MSProject.Api" },
@@ -21,6 +26,10 @@ public class ArquiteturaTests
         new object[] { AssemblyAplicacao, AssemblyInfraestrutura.GetName().Name },
         new object[] { AssemblyAplicacao, "MSProject.Api" },
         new object[] { AssemblyInfraestrutura, "MSProject.Api" },
+        new object[] { AssemblyComum, "MSProject.Api" },
+        new object[] { AssemblyComum, AssemblyDominio.GetName().Name },
+        new object[] { AssemblyComum, AssemblyAplicacao.GetName().Name },
+        new object[] { AssemblyComum, AssemblyInfraestrutura.GetName().Name },
     };
 
     [Theory]
