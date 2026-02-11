be9da6a4814cb1e3c93af58ee5794a498a9f4f30
William Alves <willoandrade29@gmail.com>
2026-02-11 09:32:48 -0300

    refactor: adiciona método pra verificar existência do Cnpj && renomeia método

--- MSAProject.Domain/Cliente/Persistencia/IClienteRepository.cs
+++ MSAProject.Domain/Cliente/Persistencia/IClienteRepository.cs
@@ -3,5 +3,6 @@ namespace MSAProject.Domain.Cliente.Persistencia;
 public interface IClienteRepository
 {
     Task<Cliente?> ObtemClientePorIdAsync(int id, CancellationToken token);
-    Task CriarClienteAsync(Cliente cliente, CancellationToken token);
+    Task CriaClienteAsync(Cliente cliente, CancellationToken token);
+    Task<bool> ExisteClientePorCnpjAsync(Cnpj cnpj, CancellationToken token);
 }
