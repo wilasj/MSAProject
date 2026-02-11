8fea9e25f03ee46ec9d0d31d044e8bbccaba9a5c
William Alves <willoandrade29@gmail.com>
2026-02-10 23:01:42 -0300

    feat: adiciona middleware e infraestrutura ao composition root

--- MSAProject.Api/Program.cs
+++ MSAProject.Api/Program.cs
@@ -1,5 +1,13 @@
+using MSAProject.Api.Middlewares;
+using MSAProject.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AdicionaInfraestrutura();
+
 var app = builder.Build();
+
+app.UseMiddleware<UnitOfWorkMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
