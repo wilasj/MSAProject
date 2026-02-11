3a5dfcafc5df22feceb6590f05ed392be3ffbd71
William Alves <willoandrade29@gmail.com>
2026-02-11 10:46:21 -0300

    refactor: adiciona o cancellation token nos query handlers

--- MSAProject.Application/Interfaces/IQueryHandler.cs
+++ MSAProject.Application/Interfaces/IQueryHandler.cs
@@ -4,10 +4,10 @@ namespace MSAProject.Application.Interfaces;
 
 public interface IQueryHandler<TQuery> where TQuery : IQuery
 {
-    Task<Resultado> Handle(TQuery command);
+    Task<Resultado> Handle(TQuery command, CancellationToken token);
 }
 
 public interface IQueryHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
 {
-    Task<Resultado<TResponse>> Handle(TQuery command);
+    Task<Resultado<TResponse>> Handle(TQuery command, CancellationToken token);
 }
